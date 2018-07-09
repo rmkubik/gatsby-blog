121fab66f6da349bd56a49f13a43ad61e22a0c36
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-07-08 17:55:14 -0700

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>
+          Lorem ipsum dolor amet +1 gentrify synth dreamcatcher, DIY pug
+          polaroid you probably haven't heard of them tousled street art
+          slow-carb air plant cornhole authentic brunch. Coloring book vinyl art
+          party subway tile gochujang meggings listicle. Umami wayfarers prism,
+          godard disrupt hella before they sold out portland slow-carb.
+          Authentic narwhal tofu mumblecore disrupt.
+        </p>
+      </div>
+    )
+  }
+}
