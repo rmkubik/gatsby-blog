b31cd27aa182206758dc2d64d5442b5e34bebef1
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-07-09 22:52:03 -0700

    Use image sharp gatsby plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Img from 'gatsby-image'
 
 const IndexPage = ({ data }) => (
   <div>
     <h1>Hello World</h1>
+    <Img sizes={data.background.sizes} />
     <p>{data.site.siteMetadata.title}</p>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -17,6 +19,11 @@ query SiteMeta {
         siteMetadata {
           title
           desc
+        }
+    }
+    background: imageSharp(id :{regex:"/bg.jpeg/"}) {
+        sizes(maxWidth: 1240) {
+            ...GatsbyImageSharpSizes
         }
     }
 }
