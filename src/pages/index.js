632a3cb6837ae3d6466f150560b46bfa70b1bd5f
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-07-09 22:29:18 -0700

    Graphql data layer test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <div>
     <h1>Hello World</h1>
+    <p>{data.site.siteMetadata.title}</p>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
   </div>
 )
 
 export default IndexPage
+
+export const query = graphql`
+query SiteMeta {
+    site {
+        siteMetadata {
+          title
+          desc
+        }
+    }
+}
+`
