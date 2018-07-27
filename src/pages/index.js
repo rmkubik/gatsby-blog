2f48d0a5713d1ab0d5603d6a1c53b317420657d0
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-07-26 23:37:52 -0700

    Create post page to display given blog post

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,6 +31,9 @@ export const query = graphql`
             title
             date(formatString: "d/M/YY")
           }
+          fields {
+            slug
+          }
         }
       }
     }
